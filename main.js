32fea14d7bfc2cc01d4f3aa7dc66dcedc03410a0
Flowers1-Mishtu <87982268+Flowers1-Mishtu@users.noreply.github.com>
2023-09-12 19:04:47 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -50,7 +50,8 @@ camera = document.getElementById("camera");
 function take_snapshot()
 {
   Webcam.snap(function(data_uri){
-    document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    document.getElementById("result1").innerHTML = '<img id="selfie_image1" src="'+data_uri+'">';
+   
   });
 }
 
